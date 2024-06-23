bbfaf49a897481f048b4b2cde6a4e002587f248d
Fábio Ximenes <43092867+FabioXimenes@users.noreply.github.com>
2024-06-23 15:04:49 -0300

    feat: add equatable

--- lib/app/features/authentication/models/user.dart
+++ lib/app/features/authentication/models/user.dart
@@ -1,16 +1,20 @@
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'user.g.dart';
 
 @JsonSerializable()
-class User {
+class User extends Equatable {
   final String name;
   final String email;
   final String? token;
 
-  User({required this.name, required this.email, required this.token});
+  const User({required this.name, required this.email, required this.token});
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
   Map<String, dynamic> toJson() => _$UserToJson(this);
+
+  @override
+  List<Object?> get props => [name, email, token];
 }
